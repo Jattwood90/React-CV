f1c733b9d487e504172992c459ab00d4c67ae1eb
System Administrator <root@joes-mbp.home>
2020-11-29 17:51:03 +0000

    porfolio error change

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -8,19 +8,11 @@ export default class ContactUs extends Component {
       
       <section id="contact">
         <div className='container'>
-<<<<<<< HEAD
-            
               <p>
                 <a href="mailto:joedevattwood@gmail.com"><img src='images/email.png'/></a>
               </p>
-          
-=======
-          <br>
-            <h3> 
-              <a href="mailto: joseph.p.attwood@gmail.com">Contact me!</a>
-            </h3>
-          <br>
->>>>>>> bb43404a4a7cd9286f80c0ccc674dbdf247c9f04
+          <br/>
+          <br/>
         </div>
        </section>
         );
